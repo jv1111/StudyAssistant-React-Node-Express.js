28703592a40d77678e79aa7db4ec5effb50d975c
jeremy <webd0829@gmail.com>
2023-03-27 08:06:42 +0800

    react search

--- client/src/components/ItemContainer.jsx
+++ client/src/components/ItemContainer.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import { Search } from "react-bootstrap-icons";
 
 const ItemContainer = ({ children, title }) => {
+    const [searchVal, setSearchVal] = useState();
     return (
         <div className="itemContainer">
             <div className="topDescription">
                 <h2 className="text-fam-kavoon">{title}</h2>
                 <div className="line"></div>
+                <div className="searchBox">
+                    <Search className="searchIcon" />
+                    <input type="text" name="search" placeholder="search" />
+                </div>
             </div>
             <div className="itemPanel">
                 {children}
