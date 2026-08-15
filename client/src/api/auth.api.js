9ac3c212924df0501af85daca24d2ba63c414397
jv1111 <webd0829@gmail.com>
2026-08-15 18:41:28 +0800

    format project files with prettier

--- client/src/api/auth.api.js
+++ client/src/api/auth.api.js
@@ -1,30 +1,30 @@
-  import api from "./axios";
+import api from "./axios";
 
-  const loginAPI = async (usernameOrEmail, password) => {
-    const response = await api.post("/auth/login", {
-      usernameOrEmail,
-      password,
-    });
+const loginAPI = async (usernameOrEmail, password) => {
+  const response = await api.post("/auth/login", {
+    usernameOrEmail,
+    password,
+  });
 
-    return response.data;
-  };
+  return response.data;
+};
 
-  const signUpAPI = async (userData) => {
-    const response = await api.post("/auth/register", userData);
+const signUpAPI = async (userData) => {
+  const response = await api.post("/auth/register", userData);
 
-    return response.data;
-  };
+  return response.data;
+};
 
-  const getSessionAPI = async () => {
-    const response = await api.get("/auth/login");
+const getSessionAPI = async () => {
+  const response = await api.get("/auth/login");
 
-    return response.data;
-  };
+  return response.data;
+};
 
-  const logoutAPI = async () => {
-    const response = await api.get("/auth/logout");
+const logoutAPI = async () => {
+  const response = await api.get("/auth/logout");
 
-    return response.data;
-  };
+  return response.data;
+};
 
-  export { loginAPI, signUpAPI, getSessionAPI, logoutAPI };
+export { loginAPI, signUpAPI, getSessionAPI, logoutAPI };
