7cd166d6fd7c613809f4b1f4799712582c9f17f6
jv1111 <webd0829@gmail.com>
2026-09-04 13:12:24 +0800

    style: format code with prettier

--- server/src/services/quizRecord.service.js
+++ server/src/services/quizRecord.service.js
@@ -1,70 +1,70 @@
-  const QuizRecord = require("../models/quizRecords.model");
+const QuizRecord = require("../models/quizRecords.model");
 
-  const AppError = require("../utils/AppError");
-  const { getPagination } = require("../utils/pagination");
+const AppError = require("../utils/AppError");
+const { getPagination } = require("../utils/pagination");
 
-  const saveRecord = async (session, quiz) => {
-    const items = session.answers.map((sessionAnswer) => {
-      const quizItem = quiz.items.id(sessionAnswer.itemId);
+const saveRecord = async (session, quiz) => {
+  const items = session.answers.map((sessionAnswer) => {
+    const quizItem = quiz.items.id(sessionAnswer.itemId);
 
-      return {
-        question: quizItem.question,
-        answer: sessionAnswer.answer,
-        correctAnswer: quizItem.answer,
-        correct: sessionAnswer.correct,
-        answeredAt: sessionAnswer.answeredAt,
-      };
-    });
-
-    return QuizRecord.create({
-      userId: session.userId,
-      quizId: session.quizId,
-      quizName: quiz.quizName,
-      subject: quiz.subjectId.name,
-      quizType: session.quizType,
-      score: session.score,
-      numberOfItems: quiz.numberOfItems,
-      items,
-      completedAt: session.completedAt,
-    });
-  };
-
-  const getRecords = async (userId, searchQuery, skipCount = 0) => {
-    const filter = {
-      userId,
+    return {
+      question: quizItem.question,
+      answer: sessionAnswer.answer,
+      correctAnswer: quizItem.answer,
+      correct: sessionAnswer.correct,
+      answeredAt: sessionAnswer.answeredAt,
     };
+  });
 
-    if (searchQuery) {
-      filter.quizName = {
-        $regex: new RegExp(searchQuery, "i"),
-      };
-    }
+  return QuizRecord.create({
+    userId: session.userId,
+    quizId: session.quizId,
+    quizName: quiz.quizName,
+    subject: quiz.subjectId.name,
+    quizType: session.quizType,
+    score: session.score,
+    numberOfItems: quiz.numberOfItems,
+    items,
+    completedAt: session.completedAt,
+  });
+};
 
-    const { skip, limit } = getPagination(skipCount);
-
-    const records = await QuizRecord.find(filter)
-      .sort({ completedAt: -1 })
-      .skip(skip)
-      .limit(limit);
-
-    return records;
+const getRecords = async (userId, searchQuery, skipCount = 0) => {
+  const filter = {
+    userId,
   };
 
-  const getRecordByRecordId = async (userId, recordId) => {
-    const record = await QuizRecord.findOne({
-      _id: recordId,
-      userId,
-    });
+  if (searchQuery) {
+    filter.quizName = {
+      $regex: new RegExp(searchQuery, "i"),
+    };
+  }
 
-    if (!record) {
-      throw new AppError("Quiz record not found", 404);
-    }
+  const { skip, limit } = getPagination(skipCount);
 
-    return record;
-  };
+  const records = await QuizRecord.find(filter)
+    .sort({ completedAt: -1 })
+    .skip(skip)
+    .limit(limit);
 
-  module.exports = {
-    saveRecord,
-    getRecords,
-    getRecordByRecordId,
-  };
+  return records;
+};
+
+const getRecordByRecordId = async (userId, recordId) => {
+  const record = await QuizRecord.findOne({
+    _id: recordId,
+    userId,
+  });
+
+  if (!record) {
+    throw new AppError("Quiz record not found", 404);
+  }
+
+  return record;
+};
+
+module.exports = {
+  saveRecord,
+  getRecords,
+  getRecordByRecordId,
+};
